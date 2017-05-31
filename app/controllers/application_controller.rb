99a86495b7293f2952a7b4b875991b2d44c6bfc4
Kozlov Evgeny <eeeeevgen@gmail.com>
2017-05-31 14:27:14 +0300

    cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def hello
-    render text: "Сережа лошото!"
-  end
-
-  def goodbye
-    render text: "goodbye!"
-  end
+  include SessionsHelper
 end
